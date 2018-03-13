7e04db7e53edc8eb460ededed6a2c2f3796b77af
Mark Rogaski <mrogaski@pobox.com>
2018-03-12 20:48:45 -0400

    Added get_published and unit tests.

--- umami/models.py
+++ umami/models.py
@@ -9,6 +9,24 @@ from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 
 
+class BulletinManager(models.Manager):
+    def get_published(self, group_size=None):
+        """
+
+        Args:
+            group_size (optional): Active bulletins will be returned as a list of group_size arity tuples.
+
+        Returns:
+            list: Either a list of bulletins or a list of bulletin tuples.
+
+        """
+        publist = [x for x in self.get_queryset().filter(publish=True)]
+        if group_size is None:
+            return publist
+        else:
+            return [tuple(publist[i:i + group_size]) for i in range(0, len(publist), group_size)]
+
+
 class Bulletin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     version = AutoIncVersionField()
@@ -18,6 +36,8 @@ class Bulletin(models.Model):
     published = models.DateTimeField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    objects = BulletinManager()
 
     class Meta:
         verbose_name = _('bulletin')
