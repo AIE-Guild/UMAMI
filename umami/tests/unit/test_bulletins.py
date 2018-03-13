7b2c9318bb69a1156bb159194fd57173cf7b83f9
Mark Rogaski <mrogaski@pobox.com>
2018-03-12 22:46:50 -0400

    Replaced manager-based grouping with a custom templatetag.

--- umami/tests/unit/test_bulletins.py
+++ umami/tests/unit/test_bulletins.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-import pytest
-
 from umami.models import Bulletin
+
 
 def test_bulletin_creation():
     bulletin = Bulletin.objects.create(title='Test Bulletin', body='# Spam')
@@ -14,16 +13,10 @@ def test_bulletin_creation():
     assert not bulletin.publish
     assert bulletin.published is None
 
+
 def test_bulletin_publish():
     bulletin = Bulletin.objects.create(title='Test Bulletin', body='# Spam')
     bulletin.publish = True
     bulletin.save()
     assert bulletin.publish
     assert isinstance(bulletin.published, datetime)
-
-def test_get_published():
-    for i in range(20):
-        Bulletin.objects.create(title=str(i), body='content', publish=bool(i % 2))
-    assert len(Bulletin.objects.get_published()) == 10
-    assert len(Bulletin.objects.get_published(3)) == 4
-    assert all([len(x) == 5 for x in Bulletin.objects.get_published(5)])
